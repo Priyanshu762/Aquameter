a316232145da575c412e371cd70e850dad3ebdd7
Priyanshu Kumar <kumarpiyanshu762@gmail.com>
2025-01-31 01:38:22 +0530

    Vite cinfigured

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // Allow external access
+    port: 5173,       // Ensure the port matches the one you want
+  }
 })
